cb89675f15e896a09babd30285eebc2e18c1afa3
yashraikwar12 <yashraikwar83058@gmail.com>
2025-11-11 23:57:45 +0530

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="flex items-center justify-center w-full  h-[80vh] text-6xl bg-gray-800 ">
-      Welcome to Home Page
+    <div className="flex flex-col items-center justify-center w-full  h-[80vh] text-6xl bg-gray-800 ">
+      <div> Welcome to Home Page </div>
+      <Link to='/' className="mt-15 px-4 py-2 text-2xl rounded-lg active:scale-95 bg-gray-900">
+        Click Here
+      </Link>
     </div>
   );
 };
